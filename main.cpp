389ec60e65a504afe2ba4e147ec53f58a638e30a
Bum Kim <bumcrystlbum@gmail.com>
2025-08-12 14:02:34 -0400

    ignoring build dir and removing some stuff

--- main.cpp
+++ main.cpp
@@ -50,14 +50,12 @@ int main() {
 
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-  glEnable(GL_CULL_FACE);
   glEnable(GL_BLEND);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); 
 
   Shader shader("text.vs", "text.fs");
   glm::mat4 projection = glm::ortho(0.0f, 800.0f, 0.0f, 600.0f);
   shader.use();
-  glUniformMatrix4fv(glGetUniformLocation(shader.ID, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
 
   FT_Library ft;
   if (FT_Init_FreeType(&ft)) {
@@ -67,45 +65,43 @@ int main() {
   }
 
   FT_Face face;
-  if (FT_New_Face(ft, "/usr/share/fonts/fira-code/FiraCode-Regular.ttf", 0,
-                  &face)) {
-    std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
-    return -1;
-  } else {
-    
-    FT_Set_Pixel_Sizes(face, 0, 48);
-    
-    glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // disable byte-alignment restriction
-    
-    for (unsigned char c = 0; c < 128; c++) {
-      // load character glyph
-      if (FT_Load_Char(face, c, FT_LOAD_RENDER)) {
-	std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
-	continue;
-      }
-      // generate texture
-      unsigned int texture;
-      glGenTextures(1, &texture);
-      glBindTexture(GL_TEXTURE_2D, texture);
-      glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, face->glyph->bitmap.width,
-                   face->glyph->bitmap.rows, 0, GL_RED, GL_UNSIGNED_BYTE,
-                   face->glyph->bitmap.buffer);
-      // set texture options
-      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-      // now store character for later use
-      Character character = {
-        texture,
-        glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
-        glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
-        static_cast<unsigned int>(face->glyph->advance.x)};
-      Characters.insert(std::pair<char, Character>(c, character));
+  if (FT_New_Face(ft, "/usr/share/fonts/dejavu-sans-fonts/DejaVuSans.ttf", 0, &face))
+    {
+      std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;  
+      return -1;
     }
-    glBindTexture(GL_TEXTURE_2D, 0);
-  }
 
+  FT_Set_Pixel_Sizes(face, 0, 48);
+  
+  glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // disable byte-alignment restriction
+  
+  for (unsigned char c = 0; c < 128; c++) {
+    // load character glyph
+    if (FT_Load_Char(face, c, FT_LOAD_RENDER)) {
+      std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+      continue;
+    }
+    // generate texture
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, face->glyph->bitmap.width,
+                 face->glyph->bitmap.rows, 0, GL_RED, GL_UNSIGNED_BYTE,
+                 face->glyph->bitmap.buffer);
+    // set texture options
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // now store character for later use
+    Character character = {
+      texture,
+      glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+      glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+      static_cast<unsigned int>(face->glyph->advance.x)
+    };
+    Characters.insert(std::pair<char, Character>(c, character));
+  }
 
   FT_Done_Face(face);
   FT_Done_FreeType(ft);
@@ -129,7 +125,7 @@ int main() {
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    RenderText(shader, "This is sample text", 25.0f, 25.0f, 1.0f, glm::vec3(0.5, 0.8f, 0.2f));
+    RenderText(shader, "This is sample text", 25.0f, 550.0f, 1.0f, glm::vec3(0.5, 0.8f, 0.8f));
 
     glfwSwapBuffers(window);
     glfwPollEvents();
